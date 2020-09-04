04ba848b029e3ae0edb851b52de89949715bdb65
tuyen <camgiacmanh1999@gmail.com>
2020-09-04 18:18:18 +0700

    update code

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const express = require('express')
 const ap = express()
 const path = require('path')
 
+//ap.get('/',function(req,res){
+//    var duongDanFile = path.join(__dirname,'home.html')
+//    res.sendfile(duongDanFile)
+//})
 ap.get('/',function(req,res){
-    var duongDanFile = path.join(__dirname,'home.html')
-    res.sendfile(duongDanFile)
+    res.json('hello')
 })
 
-ap.listen('3500', function(req,res){
-    
+ap.listen(process.env.PORT, function(req,res){
+
 })
